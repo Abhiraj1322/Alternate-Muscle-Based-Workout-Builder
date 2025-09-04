a2c362a3e9824a39320cd9a96883e8dc8a3e15d2
abhiraj1322 <abhirajdogra2@gmail.com>
2025-09-04 01:11:38 -0400

    backenfd fix

--- Server/multer.js
+++ Server/multer.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const exerciseName = req.body.name;
-    const uploadPath = path.join(__dirname, "..", "public", exerciseName);
+    const uploadPath = path.join(__dirname,  "public", exerciseName);
 
 
     if (!fs.existsSync(uploadPath)) {
